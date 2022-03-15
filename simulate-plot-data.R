abba332d8fd30c5f7b2d4cf438febf1a53c6e6d6
Stephanie Doner <shampton@email.arizona.edu>
2022-03-15 10:27:32 -0700

    add outline for data simulation

---
+++ simulate-plot-data.R
@@ -0,0 +1,10 @@
+#simulate and plot data
+#Stephanie Doner
+#shampton@email.arizona.edu
+#2022.03.15
+
+#simulate predictor variable (x)
+
+#calculate response variable (y) and add noise
+
+#plot the data
